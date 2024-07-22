05e434d67e026170cd5fbcbbf716434356caccea
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-07-22 23:32:33 +0100

    Added the update, and delete a single element

--- src/main/java/com/techieemma/learning/FirstController.java
+++ src/main/java/com/techieemma/learning/FirstController.java
@@ -1,7 +1,9 @@
 package com.techieemma.learning;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -37,5 +39,40 @@ public class FirstController {
             @PathVariable("name") String name
     ) {
         return repository.findAllByFirstNameContaining(name.toLowerCase());
+    }
+
+    @PutMapping("students/{id}")
+    public Student updateStudentById(
+            @PathVariable("id") Integer id,
+            @RequestBody Student updatedStudent
+    ) {
+        return repository.findById(id)
+                .map(student -> {
+                    if (updatedStudent.getFirstName() != null) {
+                        student.setFirstName(updatedStudent.getFirstName());
+                    }
+                    if (updatedStudent.getLastName() != null) {
+                        student.setLastName(updatedStudent.getLastName());
+                    }
+                    if (updatedStudent.getAge() != 0) { // Assuming age 0 is not a valid age
+                        student.setAge(updatedStudent.getAge());
+                    }
+                    return repository.save(student);
+                })
+                .orElseGet(() -> {
+                    updatedStudent.setId(id);
+                    return repository.save(updatedStudent);
+                });
+
+    }
+
+    @DeleteMapping("students/{id}")
+    public List<Student> deleteStudentById(
+            @PathVariable("id") Integer id
+    ) {
+        return Collections.singletonList(repository.findById(id).map(student -> {
+            repository.deleteById(id);
+            return student;
+        }).orElse(null));
     }
 }
